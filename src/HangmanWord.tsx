0273285adb24de44b837f4e665e329190b3d12d2
Femi <fagbemibabs18@gmail.com>
2025-04-15 22:50:48 +0100

    Added guessed word space

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,18 @@
 export function HangmanWord() {
-    
+    const word = "test"
+    return <div style = {{
+        display: "flex",
+        gap: "0.25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace"
+    }}>
+        {word.split("").map((letter, index) => (
+            <span style = {{
+                borderBottom: "0.1em solid black"
+            }}>{letter}</span>
+            
+        ))}
+    </div>
 }
